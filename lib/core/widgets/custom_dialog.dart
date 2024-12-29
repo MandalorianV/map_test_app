848d19b0ddce5c28668343f0224509f80447d383
Mandalorian <akifsayrim@gmail.com>
2024-12-29 20:59:49 +0300

    linter rules applied

--- lib/core/widgets/custom_dialog.dart
+++ lib/core/widgets/custom_dialog.dart
@@ -1,42 +1,45 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-Future<void> showCustomDialog(
-    {required String descriptionText,
-    required String title,
-    required Function() onPressedOK,
-    required String buttonTextOK,
-    required Function() onPressedCancel,
-    required String buttonTextCancel,
-    required BuildContext context}) async {
+Future<void> showCustomDialog({
+  required String descriptionText,
+  required String title,
+  required Function() onPressedOK,
+  required String buttonTextOK,
+  required Function() onPressedCancel,
+  required String buttonTextCancel,
+  required BuildContext context,
+}) async {
   await showAdaptiveDialog(
-      context: context,
-      builder: (context) => AlertDialog.adaptive(
-            title: Text(title),
-            content: Text(descriptionText),
-            actions: [
-              adaptiveAction(
-                context: context,
-                onPressed: onPressedOK,
-                child: Text(
-                  buttonTextOK,
-                ),
-              ),
-              adaptiveAction(
-                context: context,
-                onPressed: onPressedCancel,
-                child: Text(
-                  buttonTextCancel,
-                ),
-              )
-            ],
-          ));
+    context: context,
+    builder: (BuildContext context) => AlertDialog.adaptive(
+      title: Text(title),
+      content: Text(descriptionText),
+      actions: <Widget>[
+        adaptiveAction(
+          context: context,
+          onPressed: onPressedOK,
+          child: Text(
+            buttonTextOK,
+          ),
+        ),
+        adaptiveAction(
+          context: context,
+          onPressed: onPressedCancel,
+          child: Text(
+            buttonTextCancel,
+          ),
+        ),
+      ],
+    ),
+  );
 }
 
-Widget adaptiveAction(
-    {required BuildContext context,
-    required VoidCallback onPressed,
-    required Widget child}) {
+Widget adaptiveAction({
+  required BuildContext context,
+  required VoidCallback onPressed,
+  required Widget child,
+}) {
   final ThemeData theme = Theme.of(context);
   switch (theme.platform) {
     case TargetPlatform.android:
